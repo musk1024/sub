7d2a86e99e2e20dba34d50af3242ef0cc90eafb5
Musk <wjrwxxxxs@126.com>
2025-08-19 21:44:27 +0800

    Update

--- start.sh
+++ start.sh
@@ -3,12 +3,25 @@ if [ ! -f /usr/share/nginx/html/conf/config.js ]; then
   cp /app/conf/config.js /usr/share/nginx/html/conf
 fi
 
+if [ ! -f /base/pref.toml ]; then
+  cp /base/pref.example.toml /base/pref.toml
+fi
+
 if [ $API_URL ]; then
   echo "当前 API 地址为: $API_URL"
   sed -i "s#http://127.0.0.1:25500#$API_URL#g" /usr/share/nginx/html/conf/config.js
+  sed -i "s#managed_config_prefix = \"http://127.0.0.1:25500\"#managed_config_prefix = \"$API_URL\"#g" /base/pref.toml
 else
   echo "当前为默认本地 API 地址: http://127.0.0.1:25500"
   echo "如需修改请在容器启动时使用 -e API_URL='https://sub.ops.ci' 传递环境变量"
+fi
+
+if [ $EXTERNAL_CONFIG_URL ]; then
+  echo "当前外部配置文件地址为: $EXTERNAL_CONFIG_URL"
+  sed -i "s|^# default_external_config = \"config/example_external_config.toml\"|default_external_config = \"$EXTERNAL_CONFIG_URL\"|g" /base/pref.toml
+else
+  echo "当前为默认本地 External Config 地址: config/example_external_config.toml"
+  echo "如需修改请在容器启动时使用 -e EXTERNAL_CONFIG_URL='https://sub.ops.ci/config/example_external_config.toml' 传递环境变量"
 fi
 
 if [ $SHORT_URL ]; then
@@ -30,7 +43,7 @@ init_nginx (){
   sed -i '$d' /etc/nginx/conf.d/default.conf
   sed -i '$d' /etc/nginx/conf.d/default.conf
   cat >> /etc/nginx/conf.d/default.conf <<EOF
-    location ~* /(sub|render|getruleset|surge2clash|getprofile) {
+    location ~* /(sub|render|getruleset|surge2clash|getprofile|flushcache) {
         proxy_redirect off;
         proxy_pass http://127.0.0.1:25500;
     }
